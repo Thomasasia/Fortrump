7124646873df675f23d770e821e484908dc9ecf9
Thomas <thomas.harriett@gmail.com>
2017-11-20 15:34:37 -0500

    Deleting the redundant

--- main.py
+++
@@ -1,9 +0,0 @@
-import csv, random
-
-with open('tweets.csv', 'r') as csvfile:
-    tweets = csv.reader(csvfile)
-    rows = []
-    for row in tweets:
-        rows.append(row)
-    rand_index = random.randint(0,len(rows))
-    print(rows[rand_index][0])
